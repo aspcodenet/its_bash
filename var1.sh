2ec125f505d4b37d2e312496198901a53197b898
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-09-05 20:05:56 +0200

    Update var1.sh

--- var1.sh
+++ var1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Hej, vad heter du?"
 read name
-echo "Kul att träffs ${name}"
+echo Kul att träffas $name
 
 #notera skillnaden med enkelfnuttar
 #variabeln byts inte ut, strängen skrivs bokstavligt
