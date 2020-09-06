1428dec7cad64e86f107cd9152c09fd69e31dad4
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-09-06 14:54:04 +0200

    Create filesdatum2.sh

---
+++ filesdatum2.sh
@@ -0,0 +1,18 @@
+ GNU nano 2.9.3                                                     filesdatum2.sh                                                               
+while [[ "$#" > 0 ]]; do case $1 in
+  -d) d=$2; shift;shift;;
+  -f) f=$2;shift;shift;;
+ *) echo "Unknown parameter passed: $1";shift;shift;;
+esac; done
+
+FILES="$d"/*
+for fa in $FILES
+do
+        if [[ $fa == *"$f"*  ]]
+        then
+                size=$(stat -c%s "$fa")
+                echo "$fa $size"
+        fi
+done
+
+
